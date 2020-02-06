3ad2004191fc5b911e2369292607ec6915b1fad1
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-06 21:02:24 +0000

    mirror

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,15 +4,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int number;
-
-	for (number = 0; number < 10; number++)
-	{
-		putchar(number + '0');
+ * Return: ;
 	}
 	printf("\n");
 	return (0);
