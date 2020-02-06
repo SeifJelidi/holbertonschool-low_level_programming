e63d86ffa4c36a11d22bda4cf52a08fa96218cff
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-06 20:40:19 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,21 +8,12 @@
  */
 int main(void)
 {
-	int n;
+	char alphabet;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n % 10 > 5)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	putchar (alphabet);
 	}
-	if (n % 10 == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
-	else if (n % 10 < 6 && n % 10 != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
+	printf("\n");
 	return (0);
 }
