7c210af882e683dbdf0260bac0251de1d57ce341
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-11 20:52:15 +0000

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
+
 /**
  * print_alphabet - print alphabet lowercase x10
  *
  * Description: call print_alphabet() x10
  * Return: sucessful alphabet
  */
+
 void print_alphabet_x10(void)
 {
 char i;
