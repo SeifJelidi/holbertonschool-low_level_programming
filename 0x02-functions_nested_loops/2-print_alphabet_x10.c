78112808aba7cb3fcba120bad8a0e7d577107176
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-11 20:21:18 +0000

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char i;
-	int j = 0;
-
-	for (j = 1; j <= 10; j++)
-	{
-		i = 'a';
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
-	}
+char i;
+int j = 0;
+for (j = 1; j <= 10; j++)
+{
+i = 'a';
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
 }
