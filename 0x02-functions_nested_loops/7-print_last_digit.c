9dc9674eab1f63300a45c7221f845871061933aa
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-11 19:49:50 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print last_digit - works with defined prototype variables
+* Print last digit of a number
+* h : is a int type variable
+* Return: h as a last digit of a number
+*/
+
+int print_last_digit(int h)
+{
+	int a;
+
+	a = h % 10;
+	if (a < 0)
+	{
+		a = (a * -1);
+	}
+	_putchar(a + '0');
+	return (a);
+}
