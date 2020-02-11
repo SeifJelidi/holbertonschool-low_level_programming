e918062310a2da2f7a75d6f37f5966c55ad9da03
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-11 18:25:20 +0000

    check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower : checks if c is lowercase
+ * Return: 1 of 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
