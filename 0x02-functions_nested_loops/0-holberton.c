49f4b2401765069a133d430991841af24fd75566
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-11 12:48:04 +0000

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 /**
-*main - Printing holberton
-*return: 0
-*/
-
+ * main - Printing Holberton
+ *
+ * Return: 0
+ */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 return (0);
 }
