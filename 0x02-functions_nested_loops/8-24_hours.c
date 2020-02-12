8f7e0d3936a4f4e208e32a0cf80898b71249f27f
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-12 13:35:56 +0000

    update code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 /**
-*Jack_Bauer woork with prototype variables
-*Prints every minuts in day 
-*Return:minutes
+* jack_bauer - work with prototype variables
+* Prints every minuts in day
+* Return: minutes
 */
 void jack_bauer(void)
 {
-int x;
-int z;
-for (x = 0; x <24; x++)
-{
-for (z = 0; z < 60; z++)
-{
-_putchar(x / 10 + '0');
-_putchar(x % 10 + '0');
-_putchar(58);
-_putchar(z / 10 + '0');
-_putchar(z % 10 + '0');
-_putchar('\n');
+	int x;
+	int z;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (z = 0; z < 60; z++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(58);
+			_putchar(z / 10 + '0');
+			_putchar(z % 10 + '0');
+			_putchar('\n');
 		}
-	}	
-} 
+	}
+}
