b04d83767ee048694f33c4cfe3ab5345a5387690
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-13 22:35:21 +0000

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 
 /**
-  * print_square - prints square n times
-  *@n: int type variable
+  * print_square - prints square
+  *@size: int type variable
   * Return: 0
   */
 
-void print_square(int n)
+void print_square(int size)
 {
 
 	int i;
 	int j;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
