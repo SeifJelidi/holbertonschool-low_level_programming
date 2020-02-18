591de31aee85bb3daa17d7087aa61c62573c8785
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-18 12:41:29 +0000

    update code

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
-   * swap_int - swaps the values of two integers
-    *@a: int pointer type variable
-     *@b: int pointer type variable
-      * Return: Always 0.
-       */
+ * swap_int - swaps the values of two integers
+ *@a: int pointer type variable
+ *@b: int pointer type variable
+ * Return: Always 0.
+ */
 void swap_int(int *a, int *b)
+
 {
 	int s;
+
 	s = *a;
 	*a = *b;
 	*b = s;
