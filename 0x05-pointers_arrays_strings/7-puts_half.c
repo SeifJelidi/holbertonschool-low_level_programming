99cd5ad220b3e0a981279304d77c0fa6d2a83062
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-18 18:02:34 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts_half - printf half of a string
+ *@str: char pointer type variable
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int i;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	l++;
+	for (i = len / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
