465cc2fe1839986b2d69480cb0c7df8b8bf58320
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-20 19:38:01 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *  rot13 - encodes a string into rot13
+ *@s: char pointer type variable
+ * Return: char
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0 ; s[i] != 0 ; i++)
+	{
+		for (j = 0 ; a[j] != 0 ; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
