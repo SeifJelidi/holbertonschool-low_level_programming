093efd2cc9f8ac13ceb829de2b1950e131db9adb
Seif Jelidi <seifjelidi3@gmail.com>
2020-02-25 23:06:46 +0000

    fix3

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,6 +14,10 @@ char *_strstr(char *haystack, char *needle)
 	int j = 0;
 	int found = 0;
 
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
