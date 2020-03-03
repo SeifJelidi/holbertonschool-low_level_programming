65197a758df2dbae2825c1c49746c2eb5969f30a
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-03 22:03:52 +0000

    fix concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,27 +25,34 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0 ; s1[i] != '\0' ; i++)
+	if (s1 != '\0' && s2 != '\0')
 	{
-		len1++;
+		for (i = 0 ; s1[i] != '\0' ; i++)
+		{
+			len1++;
+		}
+		for (i = 0 ; s2[i] != '\0' ; i++)
+		{
+			len2++;
+		}
+		ch = malloc(sizeof(char) * (len1 + len2) + 2);
+		if (ch == '\0')
+		{
+			return ('\0');
+		}
+		for (i = 0 ; s1[i] != '\0' ; i++)
+		{
+			ch[i] = s1[i];
+		}
+		for (i = len1, j = 0 ; s2[j] != '\0' ; i++, j++)
+		{
+			ch[i] = s2[j];
+		}
+		ch[i] = '\0';
 	}
-	for (i = 0 ; s2[i] != '\0' ; i++)
+	else
 	{
-		len2++;
+		ch = "";
 	}
-	ch = malloc(sizeof(char) * (len1 + len2) + 2);
-	if (ch == '\0')
-	{
-		return ('\0');
-	}
-	for (i = 0 ; s1[i] != '\0' ; i++)
-	{
-		ch[i] = s1[i];
-	}
-	for (i = len1, j = 0 ; s2[j] != '\0' ; i++, j++)
-	{
-		ch[i] = s2[j];
-	}
-	ch[i] = '\0';
 	return (ch);
 }
