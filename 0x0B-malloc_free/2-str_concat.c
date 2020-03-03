07f998b70c48bea1311549b4260b19789d88d0d6
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-03 22:10:34 +0000

    add str_len

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * str_len - takes 1 string parameters
+ * Description: returns the length of a string
+ * @s: a type string
+ * Return: length of a string
+ */
+
+int str_len(char *s)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * str_concat - takes 2 string parameters
  * Description: concatenates two strings
  * @s1: first string
@@ -27,14 +46,8 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s1 != '\0' && s2 != '\0')
 	{
-		for (i = 0 ; s1[i] != '\0' ; i++)
-		{
-			len1++;
-		}
-		for (i = 0 ; s2[i] != '\0' ; i++)
-		{
-			len2++;
-		}
+		len1 = str_len(s1);
+		len2 = str_len(s2);
 		ch = malloc(sizeof(char) * (len1 + len2) + 2);
 		if (ch == '\0')
 		{
