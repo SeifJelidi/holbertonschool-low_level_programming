c347ddca513fd926850434b5124cf02c0484f5e7
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-09 23:10:17 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+*struct dog -prototype of object dog
+*Description:Define a new type struct dog
+*@name: name of the dog -string
+*@age: his age -float
+*@owner:his owner -string
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
