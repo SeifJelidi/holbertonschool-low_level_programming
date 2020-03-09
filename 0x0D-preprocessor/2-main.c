fa5bcb04a97f518dc3b8ed7540bede117d4b2ca2
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-09 16:33:51 +0000

    __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - display the name of thr file it was compiled from.
+*Desctiption: display the name of file.
+*Return: always 0.
+*/
+
+int main(void)
+{
+	printf(__FILE__"\n");
+	return (0);
+}
