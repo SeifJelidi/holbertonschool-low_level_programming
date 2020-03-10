e7cdc354941c782471696f096cacd37bb8eb1012
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-10 21:24:47 +0000

    fix newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,8 +64,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	lenName = lenString(name);
 	lenOwner = lenString(owner);
 
-	newDog->name = malloc(lenName + 1);
-	newDog->owner = malloc(lenOwner + 1);
+	newDog->name = malloc(lenName);
+	newDog->owner = malloc(lenOwner);
 	if (newDog->name == NULL || newDog->owner == NULL)
 	{
 		free(newDog);
