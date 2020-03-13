0572188cedf24c0d80a11a2de80cd78b7ae93d98
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-13 11:13:46 +0000

    0-sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of numbers made with variadic function
+ * Description: sum of numbers made with variadic function
+ * @n: number of parameters
+ * Return: 0 or sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int s = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(list, n);
+	for (i = 0 ; i < n ; i++)
+		s += va_arg(list, int);
+	va_end(list);
+	return (s);
+}
