5b236c56c5b2416dcf10ddf11dcb34ffd08abcdf
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-14 01:02:41 +0000

    fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,18 @@
-[2;2R[2;2Rstdlib.h>
-#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: ;
+* main - Entry point
+*
+* Return: Always 0 .
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
 	}
-	printf("\n");
-	return (0);
+		printf("\n");
+		return (0);
 }
