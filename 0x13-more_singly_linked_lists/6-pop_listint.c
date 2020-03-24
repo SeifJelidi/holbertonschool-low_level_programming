4f1a5498024b13a4e26649df602253babf1aa9c6
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-24 17:01:12 +0000

    pop the first node and return its n

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head node of a linked list
+ * Description: delete the head node of a linked list
+ * @head: double pointer to head
+ * Return: head node's data (n) or 0 if head is NULL
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *q;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+	q = *head;
+	n = q->n;
+	q = q->next;
+	free(*head);
+	return (n);
+}
