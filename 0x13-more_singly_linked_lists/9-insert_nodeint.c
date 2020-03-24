c76f12b47a75f0ab6b62a16cad38b38a4a5947c8
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-24 17:49:47 +0000

    fix insert node 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		q = q->next;
 	}
 	if (q == NULL)
+	{
+		free(node);
 		return (NULL);
+	}
 	node->next = q;
 	p->next = node;
 	return (*head);
