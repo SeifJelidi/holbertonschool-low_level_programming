395d2e1b4de2705aacea3ac326ec7145473b4bf4
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-31 18:53:34 +0000

    fix read from file 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,12 +28,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fileread == -1)
 	{
 		free(text);
+		close(filedesc);
 		return (0);
 	}
 	filewrite = write(1, text, fileread);
 	if (filewrite < 0)
 	{
 		free(text);
+		close(filedesc);
 		return (0);
 	}
 	free(text);
