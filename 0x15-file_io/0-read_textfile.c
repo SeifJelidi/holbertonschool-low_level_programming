49b2417f6276854f4b7b55607190bbd5c8a20431
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-31 18:29:22 +0000

    read from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+* read_textfile - reads a text file and prints it
+* Description: reads a text file and prints it
+* @filename: string character
+* @letters: number of letters it should read and print
+* Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filedesc, fileread, filewrite;
+	char *text;
+
+	if (filename == NULL)
+		return (0);
+	filedesc = open(filename, O_RDWR);
+	if (filedesc < 0)
+		return (0);
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+		return (0);
+	fileread = read(filedesc, text, letters);
+	if (fileread < 0)
+		return (0);
+	filewrite = write(1, text, fileread);
+	if (filewrite < 0)
+		return (0);
+	free(text);
+	close(filedesc);
+	return (filewrite);
+}
