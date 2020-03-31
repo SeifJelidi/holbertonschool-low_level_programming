ba41edcc677456c7dd6befe704ea27fac2b6f2c9
Seif Jelidi <seifjelidi3@gmail.com>
2020-03-31 21:16:03 +0000

    copy a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file
+ * Description: copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to, fileread, filewrite;
+	char text[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((fileread = read(file_from, text, 1024)) > 0)
+	{
+		if (fileread == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		filewrite = write(file_to, text, fileread);
+		if (filewrite == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	fileread = close(file_from);
+	if (fileread == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	filewrite = close(file_to);
+	if (filewrite == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	return (0);
+}
