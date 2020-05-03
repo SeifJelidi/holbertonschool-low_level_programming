10c173d8ffe53cc3cb224567ba274d0659c8bfec
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-03 18:12:08 +0000

    fix getting node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * Description: getting the nth node of a dlistint_t linked list.
  * @head: pointer to head node
  * @index: unsigned int
- * Return: nth node given in index of NULL if it doesn't exist.
+ * Return: NULL if the node does not exist
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
