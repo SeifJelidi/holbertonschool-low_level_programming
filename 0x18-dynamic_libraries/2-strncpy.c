9c5b70e652ae530d233381341c4375f35583f004
Seif Jelidi <seifjelidi3@gmail.com>
2020-05-04 07:34:27 +0000

    creation of dynamic lib

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ *@dest: char pointer type variable
+ *@src: char pointer type variable
+ *@n: int type variable
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
